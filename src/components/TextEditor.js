06154aab0e3a5f15f3a08c7667ab3e3dfa3d7bc9
wil <wil@wil.i.am>
2020-03-02 16:59:09 -0300

    components texteditor and index added

---
+++ src/components/TextEditor.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+
+export default class TextEditor extends Component {
+	
+	state = {
+		value: ''
+	};
+
+	onChange = ({ value }) => {
+		this.setState({value});
+	};
+
+	render() {
+		return {
+			<Editor value={this.state.value} onChange={this.onChange} />
+		};
+	};
+};
+
+export default TextEditor;
